d68c8015ddad91c2e6f73d8096af5809fd41ddd0
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-06-22 18:01:19 +0700

    Update Tampilan Form Profile

---
+++ A3K_Library/Form_Profile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A3K_Library
+{
+    public partial class Form_Profile : Form
+    {
+        public Form_Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
